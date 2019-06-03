2097c25eb826bb39f33ace3d2015cbd07525121c
Brooke Kullberg and Mathew Akre <brooke@>
2019-06-03 11:30:45 -0700

    add passings tests for end-game functions

--- spec/monsterFarm-spec.js
+++ spec/monsterFarm-spec.js
@@ -7,20 +7,55 @@ describe('Monster', function() {
     monty = new Monster("Monty");
     jasmine.clock().install();
     monty.setHunger();
+    monty.setThirst();
+    monty.setSleep();
+    monty.encourageMints();
   });
 
   afterEach(function() {
     jasmine.clock().uninstall();
   });
 
-  it('should have a name and a food level of 15 when it is created', function() {
+  it('should have a name, food level of 15, thirst level of 20, and a sleep level of 10 when it is created', function() {
     expect(monty.name).toEqual("Monty");
     expect(monty.hunger).toEqual(15);
+    expect(monty.thirst).toEqual(20);
+    expect(monty.sleep).toEqual(10);
   });
 
   it('should have a hunger of 12 after 6001 milliseconds', function() {
     jasmine.clock().tick(6001);
     expect(monty.hunger).toEqual(12);
+  });
+
+  it('should have a thirst level of 18 after 6001 milliseconds', function() {
+    jasmine.clock().tick(6001);
+    expect(monty.thirst).toEqual(18);
+  });
+
+  it('should have a sleep level of 5 after 50001 milliseconds', function() {
+    jasmine.clock().tick(50001);
+    expect(monty.sleep).toEqual(5);
+  });
+
+  it('should have a sleep level of 5 after 50001 milliseconds', function() {
+    jasmine.clock().tick(50001);
+    expect(monty.sleep).toEqual(5);
+  });
+
+  it('should get very hungry if the food level drops to zero', function() {
+    monty.hunger = 0;
+    expect(monty.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should get very thirsty if the thirst level drops to zero', function() {
+    monty.thirst = 0;
+    expect(monty.didMonsterDieOfThirst()).toEqual(true);
+  });
+
+  it('should get very tired if the sleep level drops to zero', function() {
+    monty.sleep = 0;
+    expect(monty.didMonsterKillYouFromSleepCraze()).toEqual(true);
   });
 
 });
--- src/monsterFarm.js
+++ src/monsterFarm.js
@@ -5,6 +5,7 @@ export class Monster {
     this.hunger = 15;
     this.thirst = 20;
     this.sleep = 10;
+
   }
 
   setHunger() {
@@ -16,7 +17,7 @@ export class Monster {
   setThirst() {
     setInterval(() => {
       this.thirst--;
-    }, 4000);
+    }, 3000);
   }
 
   setSleep() {
@@ -27,25 +28,25 @@ export class Monster {
 
   encourageMints() {
     setTimeout(() => {
-      alert("You're doing great!");
+      return "You're doing great!";
     }, 30000);
     setTimeout(() => {
-      alert("Keep it up");
+      return "Keep it up!";
     }, 150000);
     setTimeout(() => {
-      alert("Wow! No monsters have eaten you...yet?");
+      return "Wow! No monsters have eaten you...yet?";
     }, 300000);
   }
 
   warning() {
     if (this.sleep === 1){
-      alert(`${this.name} is sleepy!!`)
+      return(`${this.name} is sleepy!!`);
     }
     if (this.hunger === 2){
-      alert(`${this.name} is hungry!!`)
+      return(`${this.name} is hungry!!`);
     }
     if (this.thirst === 5){
-      alert(`${this.name} is thursty!!`)
+      return(`${this.name} is thursty!!`);
     }
   }
 
